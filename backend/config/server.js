9960804465af32f9ba5a6e0adcd7c4935c968216
admin <admin@iMac-ITA-05.local>
2021-10-11 13:21:14 +0200

    Create Strapi starter project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '602df6adeb596796084a553aa1c9d9cc'),
+    },
+  },
+});
